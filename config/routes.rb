d14781a811c871cfef4e49423e41b29e5284a9ac
Caitlyn Khoo <caitlynkhoo@gmail.com>
2023-08-07 23:08:46 +1000

    online lecture notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Read all
+  get '/restaurants', to: 'restaurants#index'
+  # Create
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+  # Read one
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # Update
+  get '/restaurants/:id/edit', to: 'restaurants#edit'
+  patch '/restaurants/:id', to: 'restaurants#update'
+  # Delete
+  delete '/restaurants/:id', to: 'restaurants#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  # resources :restaurants
 end
